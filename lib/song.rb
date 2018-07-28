9c3ddde04600d95df75804dace31cb2d5fb9ea92
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-28 03:05:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
+    # DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
     binding.pry
